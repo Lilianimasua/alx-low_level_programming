6adea8d47102d9532674dfb5f98c3411370bcd4b
Lilianimasua <lilianimasua@gmailcom>
2023-03-30 16:15:16 -0700

    String encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* rot13 - encoder rot13
+* @s: pointer to string params
+*
+* Return: *s
+*/
+char *rot13(char *s)
+{
+	int j;
+	int k;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		for (k = 0; k < 52; k++)
+		{
+			if (s[j] == data1[k])
+			{
+				s[j] = datarot[k];
+				break;
+			}
+		}
+	}
+	return (s);
+}
