b2e38cca31db5905941f6b9229fcd4a726f46a25
Lilianimasua <lilianimasua@gmailcom>
2023-04-06 16:03:29 -0700

    Printing strings

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,11 @@
  */
 void _puts_recursion(char *s);
 {
-	if (*s == '\0')
-		return (0);
-		putchar('\n');
+	if (*s)
 	{
-	else
-	}
 		_putchar(*s);
 		_puts_recursion(s + 1);
-
+	}
+	else
+		putchar('\n');
 }
