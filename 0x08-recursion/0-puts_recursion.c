bc8e456f3fba7ab43c19eb71d2ff8f828a199a0a
Lilianimasua <lilianimasua@gmailcom>
2023-04-05 18:04:21 -0700

    Prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * _puts_recursion - prints a string, followed by a new line
- * @*s: string
+ * @s: string
  * Return: 0
  */
 void _puts_recursion(char *s)
@@ -12,7 +12,7 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 	else
-	{
+
 		_putchar('\n');
-	}
+
 }
