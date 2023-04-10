9ae2e234a40399f5f3e63ce80a705c041453611a
Lilianimasua <lilianimasua@gmailcom>
2023-04-10 13:39:58 -0700

    Multiplies 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -39,3 +39,25 @@ int _atoi(char *s)
 		return (0);
 		return (o);
 }
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, dig1, dig2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	dig1 = _atoi(argv[1]);
+	dig2 = _atoi(argv[2]);
+	result = dig1 * dig2;
+		printf("%d\n", result);
+		return (0);
+}
